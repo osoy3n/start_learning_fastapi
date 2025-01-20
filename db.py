edc90126c1fa72dd51e658281cd07a4b0824e90f
Yohan D Osorio L <yooslzn1487@gmail.com>
2025-01-19 22:50:48 -0500

    Add database initialization and session management

---
+++ db.py
@@ -0,0 +1,19 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from sqlmodel import SQLModel, Session, create_engine
+
+sqlite_name = "db.sqlite3"
+sqlite_url = f"sqlite:///{sqlite_name}"
+
+engine = create_engine(sqlite_url)
+
+def init_db(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDependency = Annotated[Session, Depends(get_session)]
