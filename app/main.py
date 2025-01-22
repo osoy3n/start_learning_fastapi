fab44be34d939a60020a66f27d1610c905118917
Yohan D Osorio L <yooslzn1487@gmail.com>
2025-01-21 23:28:35 -0500

    Add plan routers to main application

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from sqlmodel import select
 
 from db import init_db
-from .routers import customer_routers, transaction_routers
+from .routers import customer_routers
+from .routers import plan_routers
+from .routers import transaction_routers
 
 app = FastAPI(lifespan=init_db)
 
@@ -11,4 +13,5 @@ async def root():
     return {"message": "Hello World"}
 
 app.include_router(customer_routers.router, tags=["customers"])
+app.include_router(plan_routers.router, tags=["plans"])
 app.include_router(transaction_routers.router, tags=["transactions"])
