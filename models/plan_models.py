b838993234e5dad629d8dc6a9353a12e831fe5e8
Yohan D Osorio L <yooslzn1487@gmail.com>
2025-01-21 23:27:12 -0500

    Add plan models with relationships to customers

---
+++ models/plan_models.py
@@ -0,0 +1,20 @@
+from sqlmodel import SQLModel, Field, Relationship
+from typing import TYPE_CHECKING
+from uuid import UUID
+
+if TYPE_CHECKING:
+    from models.customer_models import Customers
+
+class CustomerPlan(SQLModel, table=True):
+    id: UUID = Field(primary_key=True)
+    customer_id: UUID = Field(foreign_key="customers.id", primary_key=True)
+    plan_id: UUID = Field(foreign_key="plans.id", primary_key=True)
+
+class Plan(SQLModel):
+    description: str = Field(default=None)
+    name: str = Field(max_length=100)
+    price: float = Field(default=None)
+
+class Plans(Plan, table=True):
+    id: UUID = Field(primary_key=True)
+    customers: list["Customers"] = Relationship(back_populates="plans", link_model=CustomerPlan)
