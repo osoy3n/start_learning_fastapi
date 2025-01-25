d74f4a22b181fbde7141cb0a89b8e4e69d587fe7
Yohan D Osorio L <yooslzn1487@gmail.com>
2025-01-25 16:16:34 -0500

    Add subscription and retrieval endpoints for customer plans with status filtering

--- app/routers/customer_routers.py
+++ app/routers/customer_routers.py
@@ -1,10 +1,10 @@
 import uuid
 
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Query
 from sqlmodel import select
 
 from models.customer_models import Customer, Customers
-from models.plan_models import CustomerPlan, Plans
+from models.plan_models import CustomerPlan, Plans, PlanType
 from db import SessionDependency
 
 router = APIRouter()
@@ -72,14 +72,37 @@ async def delete_customer(customer_id: uuid.UUID, session: SessionDependency):
     response_model=CustomerPlan,
     status_code=status.HTTP_201_CREATED
 )
-async def subscribe_to_plan(customer_id: uuid.UUID, plan_id: uuid.UUID, session: SessionDependency):
+async def subscribe_to_plan(
+    customer_id: uuid.UUID, plan_id: uuid.UUID,
+    session: SessionDependency,
+    plan_status: PlanType = Query()
+):
     customer = await get_customer(customer_id, session)
     plan = session.get(Plans, plan_id)
     if not plan:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Plan not found")
     id = uuid.uuid4()
-    customer_plan = CustomerPlan(id=id, customer_id=customer.id, plan_id=plan.id)
+    customer_plan = CustomerPlan(id=id, customer_id=customer.id, plan_id=plan.id, status=plan_status)
     session.add(customer_plan)
     session.commit()
     session.refresh(customer_plan)
     return customer_plan
+
+@router.get(
+    "/customers/{customer_id}/plans",
+    response_model=list[CustomerPlan],
+    status_code=status.HTTP_200_OK
+)
+async def customer_plans(
+    customer_id: uuid.UUID,
+    session: SessionDependency,
+    plan_status: PlanType = Query()
+):
+    customer = await get_customer(customer_id, session)
+    query = (
+        select(CustomerPlan)
+        .where(CustomerPlan.customer_id == customer.id)
+        .where(CustomerPlan.status == plan_status)
+    )
+    plans = session.exec(query).all()
+    return plans
--- models/plan_models.py
+++ models/plan_models.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from sqlmodel import SQLModel, Field, Relationship
 from typing import TYPE_CHECKING
 from uuid import UUID
@@ -5,10 +6,15 @@ from uuid import UUID
 if TYPE_CHECKING:
     from models.customer_models import Customers
 
+class PlanType(str, Enum):
+    ACTIVE = "active"
+    INACTIVE = "inactive"
+
 class CustomerPlan(SQLModel, table=True):
     id: UUID = Field(primary_key=True)
     customer_id: UUID = Field(foreign_key="customers.id", primary_key=True)
     plan_id: UUID = Field(foreign_key="plans.id", primary_key=True)
+    status: PlanType = Field(default=PlanType.ACTIVE)
 
 class Plan(SQLModel):
     description: str = Field(default=None)
