2fe5d8b26abeebe49a6ee56ab8365d7da3c92e6d
Yohan D Osorio L <yooslzn1487@gmail.com>
2025-01-26 19:23:19 -0500

    Add email validation to Customer model to prevent duplicates

--- models/customer_models.py
+++ models/customer_models.py
@@ -1,8 +1,9 @@
-from pydantic import EmailStr
-from sqlmodel import SQLModel, Field, Relationship
+from pydantic import EmailStr, field_validator
+from sqlmodel import SQLModel, Session, Field, Relationship, select
 from typing import TYPE_CHECKING
 from uuid import UUID
 
+from db import engine
 from models.plan_models import CustomerPlan
 
 if TYPE_CHECKING:
@@ -14,6 +15,16 @@ class Customer(SQLModel):
     description: str = Field(default=None)
     email: EmailStr = Field(max_length=80)
     name: str = Field(max_length=100)
+
+    @field_validator("email")
+    @classmethod
+    def validate_email(cls, value):
+        session = Session(engine)
+        query = select(Customers).where(Customers.email == value)
+        result = session.exec(query).first()
+        if result:
+            raise ValueError("Email already exists")
+        return value
 
 class Customers(Customer, table=True):
     id: UUID = Field(primary_key=True)
